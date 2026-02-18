66e011d252b023b1e7ded6709ff93b79c55436c7
Lenin S <154426406+svlenin@users.noreply.github.com>
2026-02-19 00:01:59 +0530

    home.java

--- home.java
+++ home.java
@@ -3,7 +3,7 @@ public class home {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name:");
+        System.out.println(":");
         String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
         scanner.close();
